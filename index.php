fdf742720f2bd1884fb8417333297f71362e842c
Carlos Mario <carlos_programmer10@hotmail.com>
2015-07-13 21:03:43 -0500

    Colors

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <html>
 	<form method="post" action="calculate.php">
 		Number 1:
-		<input type="number" name="n1" style=" background: azure;"/><br>
+		<input type="number" name="n1" style=" background: green;"/><br>
 		Number 2:
-		<input type="number" name="n2" style=" background: pink;"/><br>
+		<input type="number" name="n2" style=" background: yellow;"/><br>
 		<input type="submit" value="Calculate">
 	</form>
 </html>
