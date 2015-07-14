f8a7d5c9356318defa7a327c160a9d4871cfb4a9
Carlos Mario <carlos_programmer10@hotmail.com>
2015-07-13 20:32:07 -0500

    test

---
+++ calculate.php
@@ -0,0 +1,8 @@
+<?php
+    $x = $_POST["n1"];
+    $y = $_POST["n2"];
+
+    $result = $x + $y;
+    echo "Result is:  " . $result . "<br>";
+    echo "<a href='index.html'>to the index</a>";
+?>
--- index.php
+++ index.php
@@ -1,3 +1,11 @@
-<?
-echo ":D";
-?>
+<html>
+	<form method="post" action="calculate.php">
+		Number 1:
+		<input type="number" name="n1" style=" background: azure;"/><br>
+		Number 2:
+		<input type="number" name="n2" style=" background: pink;"/><br>
+		<input type="submit" value="Calculate">
+	</form>
+</html>
+
+
