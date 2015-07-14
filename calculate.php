1af070f64d271047352ab43c065dc45dd471bf65
Carlos Mario <carlos_programmer10@hotmail.com>
2015-07-13 21:06:51 -0500

    Operation

--- calculate.php
+++ calculate.php
@@ -2,7 +2,7 @@
     $x = $_POST["n1"];
     $y = $_POST["n2"];
 
-    $result = $x + $y;
+    $result = $x - $y;
     echo "Result is:  " . $result . "<br>";
     echo "<a href='index.php'>to the index</a>";
 ?>
