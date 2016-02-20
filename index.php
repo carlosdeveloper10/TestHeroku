7b31529e24bef13a9c83ce6833ab2adaabd0df2b
Carlos Mario <carlos_programmer10@hotmail.com>
2016-02-19 20:53:21 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <html>
 	<form method="post" action="calculate.php">
 		Number 1:
-		<input type="number" name="n1" style=" background: green;"/><br>
+		<input type="number" name="n1" style=" background: red;"/><br>
 		Number 2:
 		<input type="number" name="n2" style=" background: yellow;"/><br>
 		<input type="submit" value="Calculate">
